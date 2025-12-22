cc786ef5c5153aec7e6aeedd3a3c0ef32b132b74
choiyoung0802 <mj12579632@gmail.com>
2025-12-22 17:45:11 +0900

    feat: 20251222 Java Lesson 시작

---
+++ src/main/java/ch00_starting/Main.java
@@ -0,0 +1,13 @@
+package ch00_starting;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Java"); // ctrl + d = 밑에 똑같이 복사
+        System.out.println("안근수");
+        System.out.println("123");
+        System.out.println('1');
+        System.out.println("12");
+//        System.out.println(123456);
+//        System.out.println(hello); // ctrl + / = 주석처리
+    }
+}
--- src/main/java/sh00_starting/Main.java
+++
@@ -1,7 +0,0 @@
-package sh00_starting;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello Java");
-    }
-}
