575b9311c3162226b5e1b381bcffb93e27719ca9
choiyoung0802 <mj12579632@gmail.com>
2026-01-15 12:46:36 +0900

    feat: 20260115 Java lesson done0

--- src/main/java/ch21_exception/bank/BankMain.java
+++ src/main/java/ch21_exception/bank/BankMain.java
@@ -6,8 +6,8 @@ public class BankMain {
         private int balance = 10000;
 
     public void withdraw(int amount) throws InsufficientBalanceException {
-        int gap = amount - balance;
         if (amount > balance) {
+            int gap = amount - balance;
             throw new InsufficientBalanceException("잔액이 부족합니다. 부족 금액 : " + gap + "원");
         }
     }
@@ -16,9 +16,11 @@ public class BankMain {
         BankMain bank = new BankMain();
         try {
             System.out.print("출금할 금액을 입력하세요 >>> ");
-            int amount = scanner.nextInt();
-            bank.withdraw(amount);
-            bank.balance = bank.balance - amount;
+            int money = scanner.nextInt();
+            bank.withdraw(money);
+            bank.balance = bank.balance - money;
+            System.out.println("정상 출금 되었습니다.");
+            System.out.println("출금 후 잔액 : " + bank.balance + "원");
         } catch (InsufficientBalanceException e) {
             System.out.println("출금액 입력 오류 : " + e.getMessage());
         }
